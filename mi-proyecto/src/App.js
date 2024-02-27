20e80a1319f82c2178c92eeceb710dc0dd438b0f
Pame2175 <pamegonza.98.pg@gmail.com>
2024-02-27 20:07:57 -0300

    gran_inversion

---
+++ mi-proyecto/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import MiComponente from './jsx/MiComponente';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      
+        
+        <MiComponente />
+      
+    </div>
+  );
+}
+
+export default App;
